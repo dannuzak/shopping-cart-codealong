7f05f7804f460c42964b8d4c18677b2dd6668204
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-04-18 20:16:59 -0500

    Added store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import { Products } from 'components/Products'
 import { Cart } from 'components/Cart'
+import { cart } from 'reducers/cart'
+import { products } from 'reducers/products'
+
+// this will combine both reducers
+const reducer = combineReducers({
+  cart: cart.reducer,
+  products: products.reducer
+})
+
+// this will create the store
+const store = configureStore({ reducer })
 
 export const App = () => (
-  <>
+  <Provider store={store}>
     <Cart />
     <Products />
-  </>
+  </Provider>
 )
